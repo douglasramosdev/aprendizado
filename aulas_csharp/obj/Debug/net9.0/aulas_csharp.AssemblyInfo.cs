f2d3fda848cd2d45a7025e87c4eec8a2a5b5dfc3
douglasramosdev <douglas.tavaresramos@gmail.com>
2025-03-17 22:21:52 -0300

    formatei o pc

--- aulas_csharp/obj/Debug/net9.0/aulas_csharp.AssemblyInfo.cs
+++ aulas_csharp/obj/Debug/net9.0/aulas_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aulas_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b30a8c106cc569e1a7fb5e57119cd959e04376b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc39500e46d3dfeee23f11ffc81dc8338d27d35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aulas_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aulas_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
